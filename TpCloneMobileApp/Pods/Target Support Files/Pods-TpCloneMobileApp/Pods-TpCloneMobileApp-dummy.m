e0a5e4554e4c0cd2cf0885c56a9460cb4686f304
TuRiven <phambatu111@gmail.com>
2024-01-31 10:16:28 +0700

    first commit

---
+++ TpCloneMobileApp/Pods/Target Support Files/Pods-TpCloneMobileApp/Pods-TpCloneMobileApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TpCloneMobileApp : NSObject
+@end
+@implementation PodsDummy_Pods_TpCloneMobileApp
+@end
---
+++ TpCloneMobileApp/TpCloneMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TpCloneMobileApp
+//
+//  Created by Tú Phạm Bá on 31/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
