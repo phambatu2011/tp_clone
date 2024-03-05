825d3acb910cce9658f0d52e300c3487acf8bb3b
TuRiven2011 <91541290+TuRiven2011@users.noreply.github.com>
2024-03-06 02:18:11 +0700

    commit

--- TpCloneMobileApp/TpCloneMobileApp/Screens/Tabbar/TabHome/TabHomeViewController.swift
+++ TpCloneMobileApp/TpCloneMobileApp/Screens/Tabbar/TabHome/TabHomeViewController.swift
@@ -1,12 +1,40 @@
 import UIKit
 
 class TabHomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var heightTopImageConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageHeader: UIImageView!
+    
+    private var originalHeight: CGFloat = 230
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
     }
+    
+    
+}
 
-
+extension TabHomeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        print(offset)
+        let defaultTop = CGFloat(0)
+        var currentTop = defaultTop
+        
+        if scrollView == self.scrollView {
+            if offset < 0 {
+                currentTop = offset
+                heightTopImageConstraint.constant = originalHeight - (offset*1.2)
+            } else {
+                heightTopImageConstraint.constant = originalHeight
+            }
+            imageTopConstraint.constant = currentTop
+        }
+    }
 }
